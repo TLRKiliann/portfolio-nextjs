1f90839f0ced712fdc3e562b575ffa1dda35beaf
TLRKiliann <philogenie@protonmail.com>
2026-03-20 11:34:53 +0100

    some changes

--- portfolio-carousel/ui/Legend.tsx
+++ portfolio-carousel/ui/Legend.tsx
@@ -2,9 +2,18 @@ export default function Legend() {
     return (
         <section className="relative py-16 text-white bg-linear-to-br from-gray-900 to-gray-800">
 
-            <h1 className="text-3xl font-bold">Mes intérêts</h1>
+            <h1 className="text-3xl font-bold">Tout est une question de logique...</h1>
 
-            <h3 className="text-2xl font-bold py-10">Derniers projets réalisés</h3>
+            <h3 className="text-2xl font-bold py-10"></h3>
+
+            <p>J'ai commencé par m'intéressé à l'Ethical Hacking, raison pour laquelle, 
+                j'accorde une grande importance à la sécurité.
+            </p>
+
+            <p>
+                Dans mes projets, il est tujours question de rester attentif aux vulnérabilités potentielles,
+                que ce soit au nivau des connections, des serveurs ou des interactions avec l'interface utilisateur.
+            </p>
 
             <p>API - MAJ des dates en tenant compte des vacances.</p>
             <p>Todo List - enregistre les données sur serveur avec ficchier downloadable sur desktop.</p>
