7c1423ddb3742f86b89d3ba2f29c4b1cda53d126
EveilHub <EveilHub@proton.me>
2026-03-24 16:52:36 +0100

    type & text created

---
+++ portfolio-carousel/lib/TextForLegend.ts
@@ -0,0 +1,59 @@
+export const textAPI_FR: string[] = [
+  "MAJ des dates tous les vendredi de la 8ème semaine de cours, pour les 8 semaines suivantes.",
+  "Remplacer les dates par '--/--/----' lors des vacances.",
+  "Reboot des dates le vendredi de la première semaine de chaque nouvelle année, pour les 8 prochaines semaines de cours."
+];
+
+export const textAPI_EN: string[] = [
+  "Dates are updated every Friday of the 8th week of classes for the following 8 weeks.",
+  "Replace dates with '--/--/----' during school breaks.",
+  "Dates are reset on the Friday of the first week of each new year for the next 8 weeks of classes."
+];
+
+export const textSPA_FR: string[] = [
+  "Afficher les tâches en fonction de leur priorité, du jour de la semaine, de la date et de l'heure.",
+  "Afficher les tâches sous forme de calendrier en fonction du numéro de la semaine de l'année, avec possibilité de les modifier.",
+  "Afficher les tâches terminées avec possibilité de les supprimer ou downloader le fichier au format .CSV",
+  "Si le navigateur est fermé, possibilité de l'ouvrir avec affichage des tâches en l'état, lors de la fermeture."
+];
+
+export const textSPA_EN: string [] = [
+  "Display tasks based on their priority, day of the week, date, and time.",
+  "Display tasks in calendar view based on the week number of the year, with the option to edit them.",
+  "Display completed tasks with the option to delete them or download the file in .CSV format",
+  "If the browser is closed, the option to reopen it with tasks displayed as they were when closed."
+];
+
+export const textsFR_Xp: string[] = [
+  `Mon premier défi a été de faire un site web hébergé chez moi, tout en étant accessible depuis l'extérieur. J'ai utilisé un raspberry pour monter ma stack LAMP et 
+    j'ai installé un certificat HTTPS, ainsi qu'un DDNS. Le tout sécurisé avec HTTPS et une configuration serveur contre les vulnérabiltés.`,
+  `La WebApp 'Time-Track' figurant ci-dessus est une application de soins infirmiers que j'ai amélioré et que j'ai faite selon ma propre vision et d'après mon expérience dans le milieu.`,
+  "Le projet 'Statistiques' a été réalisé chez It4net, à Lausanne. Je l'ai fait en 17 jours en natif (PHP + MySQL + JS + CSS).",
+  "J'ai fait 'Mon ECO Pote Game' qui est un jeu de société sur l'environnement, en tant que Bénévole.",
+  "NextJs server-action avec next-action Auth et middleware.",
+  "J'ai effectué 2 applications pour l'Eveil, dont les descriptifs figurent ci-dessous."
+];
+
+export const textsEN_Xp: string[] = [
+  `My first challenge was to create a website hosted on my own server, accessible from outside. I used a Raspberry Pi to set up my LAMP stack and installed an HTTPS 
+    certificate along with DDNS. Everything secured with HTTPS and server configuration against vulnerabilities.`,
+  `The 'Time-Track' WebApp shown above is a nursing care application that I improved and developed according to my own vision and based on my experience in the field.`,
+  "The 'Statistiques' project was completed at It4net in Lausanne. I built it in 17 days using native technologies (PHP + MySQL + JS + CSS).",
+  "I created 'Mon ECO Pote Game', an environmental board game, as a volunteer.",
+  "Next.js server actions with next-action Auth and middleware.",
+  "I developed 2 applications for Eveil, whose descriptions are below."
+];
+
+export const textBackendFR: string[] = [
+  "Mon premier site web a été fait sur Raspberry (Lamp) avec port forwarding et protocole HTTPS pour le rendre accessible en WAN et sécurisé (CSRF, XSS, firewall, MAJ, etc).",
+  "Ma première application faite en python3 utilisait le protocole SCP pour protéger la connexion SSH à mon server en LAN.",
+  "La réalisation de mes projets, se fait la plupart du temps sur mon Raspberry qui me sert de serveur LAN.",
+  "J'utilise aussi MongoDB (no code) et Vercel avec PostgreSQL."
+];
+
+export const textBackendEN: string[] = [
+  "My first website was built on a Raspberry Pi (LAMP) using port forwarding and the HTTPS protocol to make it accessible over the WAN and secure (CSRF, XSS, firewall, updates, etc.).",
+  "My first Python 3 application used the SCP protocol to secure the SSH connection to my LAN server.",
+  "I usually work on my projects on my Raspberry Pi, which I use as a LAN server.",
+  "I also use MongoDB (no-code) and Vercel with PostgreSQL."
+];
---
+++ portfolio-carousel/lib/type.ts
@@ -0,0 +1,19 @@
+export type Project = {
+  id: number;
+  title: string;
+  category: string;
+  description_FR: string;
+  description_EN: string;
+  image: string;
+  technologies: string[];
+  link: string;
+  linkGitHub?: string;
+}
+
+export type BinaryNumbersType = {
+  left: number;
+  top: number;
+  delay: number;
+  duration: number;
+  text: string;
+}
